c6add707650612d3e74d9d57a8b727af1bbc181f
Adrian Arroyo <gibbymetal@hotmail.com>
2020-06-05 23:46:23 -0500

    servidor

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const path = require('path');
  
 const app = express();
  
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
+ 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/client'));
